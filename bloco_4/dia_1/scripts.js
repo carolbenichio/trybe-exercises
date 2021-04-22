264e522aa9eee4bdcb86694a44d1110b99bca98e
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-04-22 15:34:07 -0300

    Criacao do bloco 4, dia 1 e scripts.js

---
+++ bloco_4/dia_1/scripts.js
@@ -0,0 +1,10 @@
+const name = "Caroline";
+const birthCity = "Vitória";
+let birthYear = "1996";
+
+birthYear ="2030";
+birthCity ="Vilha Velha"
+
+console.log(name);
+console.log(birthCity)
+console.log(birthYear);
