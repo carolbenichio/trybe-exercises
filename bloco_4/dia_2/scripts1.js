8773c2138b34313b9082db6418bf807c40bd536f
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-04-23 16:07:27 -0300

    Continuacao dos exemplos de For e For of

--- bloco_4/dia_2/scripts1.js
+++ bloco_4/dia_2/scripts1.js
@@ -1,5 +1,34 @@
+//ARRAYS
+//1
+
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
 console.log(menuServices);
 
+//2
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+//3 
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push("Contato");
+
+console.log(menu);
+
+//FOR
+//1
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index +=1) {
+    console.log(groceryList[index]);
+}
+
+//FOR OF
+//1
