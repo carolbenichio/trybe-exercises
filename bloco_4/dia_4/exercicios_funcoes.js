14c73b7c2955f9e672aa408784ff89729b94b687
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-04-27 21:57:25 -0300

    Exercicios funcoes 3 e 4

--- bloco_4/dia_4/exercicios_funcoes.js
+++ bloco_4/dia_4/exercicios_funcoes.js
@@ -43,3 +43,40 @@ function maior (numeros) {
 }
 
 console.log(maior(numeros));
+
+//3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+let numeros = [55, 66, 99, 22, 100];
+let posicao = numeros[0];
+
+function menor (numeros) {
+    for (let index = 0; index < (numeros.length-1); index +=1) {
+        if (numeros[index] < numeros[index+1] && numeros[index] <= posicao) {
+            posicao = numeros[index];
+        } else {
+            posicao = posicao;
+        }
+    }
+    let index = numeros.indexOf(posicao);
+    return(index);
+}
+
+console.log(menor(numeros));
+
+//4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+let nomes = ['Joana', 'Laura', 'Caroline', 'Francisquinha'];
+let maiorPalavra = nomes[0];
+
+function maior (nomes) {
+    for (let key in nomes) {
+        if (maiorPalavra.length < nomes[key].length) {
+            maiorPalavra = nomes[key];
+        }
+    }
+    return(maiorPalavra);
+}
+
+console.log(maior(nomes));
+
+//5 
