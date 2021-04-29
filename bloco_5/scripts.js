1555d826ac9023075c1913e24c00c6d12b2b18fe
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-04-29 15:51:55 -0300

    Criacao do script.html e script.js para exemplos

---
+++ bloco_5/scripts.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText = "Bastardos Inglórios";
+document.getElementById("second-paragraph").innerText = "Melhor filme do mundo!";
+document.getElementById("subtitle").innerText = "Tarantino te amo, sabe?";
