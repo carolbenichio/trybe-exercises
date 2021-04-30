47ab5fbb7ab572ab421b43b5149e226ac9430875
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-04-30 18:09:09 -0300

    Fixacao parte 2 feita

--- bloco_5/dia_2/scripts.js
+++
@@ -1,7 +0,0 @@
-//1 Acesse o elemento elementoOndeVoceEsta.
-const elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
-
-//2 Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
-
-const pai = elementoOndeVoceEsta.parentElement;
-pai.style.color = 'red';
