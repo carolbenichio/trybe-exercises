913ddf7254a78dfa7c913135b83f0f0ec72f0d4d
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-04-29 23:46:22 -0300

    Exemplos de fixacao pra aula 5.1

---
+++ bloco_5/script.js
@@ -0,0 +1,9 @@
+document.getElementById("header-container").style.backgroundColor = "#2fc18c";
+document.getElementById("footer-container").style.backgroundColor = "#006dfb";
+document.querySelector(".emergency-tasks").style.backgroundColor = "salmon";
+document.querySelector(".no-emergency-tasks").style.backgroundColor = "yellow";
+//document.getElementsByTagName("h3").style.backgroundColor = "purple";
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = "purple";
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = "purple";
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = "black";
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = "black";
