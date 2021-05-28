962c50cea567aed996296be2a98fbed5b500dc1e
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-05-28 15:10:39 -0300

    Exercicios 8.3 - Ajuste no ex 7

--- bloco_8/dia_3/exercicio7.js
+++ bloco_8/dia_3/exercicio7.js
@@ -4,7 +4,8 @@ const books = require('./books');
 const expectedResult = 'O Senhor dos Anéis';
 
 function authorWith3DotsOnName() {
-  const livro = books.find((book) => book.author.name.substring(1,2) === '.');
+  const livro = books.find((book) => 
+    book.author.name.substring(1,2) === '.' && book.author.name.substring(4,5) === '.' && book.author.name.substring(7,8) === '.');
   return livro.name;
 }
 
