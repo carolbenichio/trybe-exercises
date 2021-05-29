26f2bbcc45cca034a9ef3ab3767d1952cbf6be6c
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-05-29 11:55:20 -0300

    Ex. 4 (8.4)

--- bloco_8/dia_4/exercicio2-3-4.js
+++ bloco_8/dia_4/exercicio2-3-4.js
@@ -91,3 +91,16 @@ const books = [
 
 //4 
 
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: { name: 'George R. R. Martin', birthYear: 1948 },
+  releaseYear: 1991
+};
+
+function longestNamedBook() {
+  return books.reduce((acc, book) => (acc.name.length > book.name.length) ? acc : book);
+}
+
+assert.deepStrictEqual(longestNamedBook(), expectedResult);
