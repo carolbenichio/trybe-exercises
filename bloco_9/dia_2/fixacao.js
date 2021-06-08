873d07700319821e48b35a14f46aa81ca0800ae1
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-06-08 16:07:59 -0300

    Exemplos de fixacao pra aula 9.2

---
+++ bloco_9/dia_2/fixacao.js
@@ -0,0 +1,84 @@
+// Sem .then()
+
+const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random() * 11);
+
+  if (number <= 5) {
+    return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+  }
+  resolve(console.log(`Que sucesso =) nosso número foi ${number}`));
+}); 
+
+// Com .then()
+
+const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random() * 11);
+
+  if (number <= 5) {
+    return reject(console.log(`Que fracasso =( Nosso número foi ${number}`));
+  }
+  resolve(number);
+})
+.then(number => `Que sucesso =) nosso número foi ${number}`)
+.then(msg => console.log(msg));
+
+// Com .this() e .catch()
+
+const promise = new Promise((resolve, reject) => {
+  const number = Math.floor(Math.random()* 11);
+
+  if (number <= 5) {
+    return reject(number);
+  }
+  resolve(number);
+})
+.then(number => `Que sucesso =) nosso número foi ${number}`)
+.then(msg => console.log(msg))
+.catch(number => console.log(`Que fracasso =( Nosso número foi ${number}`));
+
+// .fetch()
+
+const fetch = require('node-fetch');
+
+function verifiedFetch(url) {
+  return new Promise((resolve, reject) => {
+    if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
+      fetch(url)
+        .then((r) => r.json())
+        .then((r) => resolve(r.value));
+    } else {
+      reject(new Error('endpoint não existe'));
+    }
+  });
+}
+
+function sendJokeToFriend(name) {
+  const message = verifiedFetch('https://api.chucknorris.io/jokes/random?category=dev')
+    .then((joke) => `Oi ${name}, ouve essa: ${joke}`)
+    .catch((err) => err);
+  console.log(message);
+}
+
+sendJokeToFriend("Anna");
+
+// Com async e await
+
+const fetch = require('node-fetch');
+
+async function verifiedFetch(url) {
+  if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
+    return fetch(url)
+      .then((r) => r.json())
+      .then((r) => (r.value));
+  }
+  throw new Error('endpoint não existe');
+}
+
+async function sendJokeToFriend(name) {
+  const message = await verifiedFetch('https://api.chucknorris.io/jokes/random?category=dev')
+    .then((joke) => `Oi ${name}, ouve essa: ${joke}`)
+    .catch((err) => err);
+  console.log(message);
+}
+
+sendJokeToFriend("Anna");
