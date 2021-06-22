66dcf765f2d574b8a1576e3c1ec780780ec302f9
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-06-22 01:15:20 -0300

    Criando fixacao e exercicios dia 11.1

---
+++ Módulo 2/bloco_11/dia_1/fixacao/exercicio-fixacao/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Fixacao1 from './Component';
+
+function App() {
+  return <Fixacao1 />;
+}
+
+export default App;
---
+++ Módulo 2/bloco_11/dia_1/fixacao/exercicio-fixacao/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Fixacao1 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Caroline Benichio Teixeira</h1>
+        <p>FullStack developer</p>
+      </div>
+    )
+  }
+}
+
+export default Fixacao1;
