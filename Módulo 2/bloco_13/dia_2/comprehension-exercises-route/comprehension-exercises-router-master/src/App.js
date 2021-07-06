760c21932ef79c13b1450a84b45d0d0d6519f072
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-07-06 17:29:38 -0300

    Exercicio 7-9

--- Módulo 2/bloco_13/dia_2/comprehension-exercises-route/comprehension-exercises-router-master/src/App.js
+++ Módulo 2/bloco_13/dia_2/comprehension-exercises-route/comprehension-exercises-router-master/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -11,9 +11,11 @@ class App extends Component {
       <Link exact to="/" > Home </Link>
       <Link to="/about" > About </Link>
       <Link to="/users" > Users </Link>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/users" component={Users} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/users/:id?" render={(props) => <Users {...props} greetingsMessage="Good Morning!" /> }/>
+        </Switch>
       </BrowserRouter>
     );
   }
--- Módulo 2/bloco_13/dia_2/comprehension-exercises-route/comprehension-exercises-router-master/src/components/Users.js
+++ Módulo 2/bloco_13/dia_2/comprehension-exercises-route/comprehension-exercises-router-master/src/components/Users.js
@@ -3,10 +3,13 @@ import {Link} from 'react-router-dom';
 
 class Users extends Component {
   render() {
+    const { greetingsMessage } = this.props;
+    const { id } = this.props.match.params;
     return (
       <div>
         <h2>Users</h2>
         <p> My awesome Users component </p>
+        <p>{ `${greetingsMessage }, My awesome ${id ? id : 'fulano'} component` }</p>
         <Link exact to="/" > Voltar para home </Link>
       </div>
     );
