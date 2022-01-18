4f2b66ead748f6f87fc6adcd26c66b0c3f7f6411
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2022-01-18 17:19:56 -0300

    Fixacao dia 3 - finaliza o chat

--- Módulo 3/bloco_30/dia_3/chat/sockets/chat.js
+++ Módulo 3/bloco_30/dia_3/chat/sockets/chat.js
@@ -1,4 +1,6 @@
 module.exports = (io) => io.on('connection', (socket) => {
+  socket.emit('msg', 'Olá, seja bem vindo ao nosso chat público! Use essa página para conversar a vontade.');
+
   socket.on('clientMessage', (message) => {
     console.log(`Mensagem ${message}`);
     io.emit('msg', message);
