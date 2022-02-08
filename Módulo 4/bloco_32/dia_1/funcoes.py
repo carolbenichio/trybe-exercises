6edd92ac9594cf537aa4d355284edebcaa5e77a2
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2022-02-08 19:32:03 -0300

    Funcoes outros exemplos

--- Módulo 4/bloco_32/dia_1/funcoes.py
+++ Módulo 4/bloco_32/dia_1/funcoes.py
@@ -11,6 +11,7 @@ def concat(*strings):
     # Equivalente a um ", ".join(strings), que concatena os elementos de um iterável em uma string utilizando um separador
     # Nesse caso a string resultante estaria separada por vírgula
     final_string = ""
+    print(strings[-1])
     for string in strings:
         final_string += string
         if not string == strings[-1]:
@@ -24,6 +25,14 @@ concat("Carlos", "João")  # saída: "Carlos, João"
 concat("Carlos", "João", "Maria")  # saída: "Carlos, João, Maria"
 
 # dict é uma função que já vem embutida no python
-dict(nome="Felipe", sobrenome="Silva", idade=25)  # cria um dicionário utilizando as chaves passadas
-
+var = dict(nome="Felipe", sobrenome="Silva", idade=25)  # cria um dicionário utilizando as chaves passadas
+print(var)
 dict(nome="Ana", sobrenome="Souza", idade=21, turma=1)  # o número de parâmetros passados para a função pode variar
+
+len([1, 2, 3, 4])  # função len não aceita argumentos nomeados
+
+len(obj=[1, 2, 3, 4])  # este código irá falhar
+
+print("Botaro", "Cássio", ", ")  # imprime Botaro Cássio ,
+
+print("Botaro", "Cássio", sep=", ")  # nomeando o terceiro parâmetro, agora temos a saída: Botaro, Cássio
