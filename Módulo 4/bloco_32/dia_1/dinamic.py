05c24b0c5c9d201132a5d6db509545199842c53f
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2022-02-08 18:36:13 -0300

    Dinamica aula

--- Módulo 4/bloco_32/dia_1/dinamic.py
+++ Módulo 4/bloco_32/dia_1/dinamic.py
@@ -3,3 +3,9 @@ tupla_dois = (lista_dois)
 
 for items in tupla_dois:
   print(len(items))
+lista_dois = ["dois", "two", "dos", 2]
+
+tupla_dois = (lista_dois)
+
+for items in tupla_dois:
+  print(len(items))
---
+++ Módulo 4/bloco_32/dia_1/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+fruits = ["orange", "apple", "grape", "pineapple"]  # elementos são definidos separados por vírgula, envolvidos por colchetes
+
+fruits[0]  # o acesso é feito por indices iniciados em 0
+
+fruits[-1]  # o acesso também pode ser negativo
+
+fruits.append("banana")  # adicionando uma nova fruta
+
+fruits.remove("pineapple")  # removendo uma fruta
+
+fruits.extend(["pear", "melon", "kiwi"])  # acrescenta uma lista de frutas a lista original
+
+fruits.index("apple")  # retorna o índice onde a fruta está localizada, neste caso em seu programa
+fruits.sort()  # ordena a lista de frutas
+
+print(fruits)
