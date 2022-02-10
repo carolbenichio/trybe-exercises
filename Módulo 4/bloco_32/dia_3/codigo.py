547a3cc74ef379bf7a4864f22f3534a7aadb0c9d
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2022-02-10 15:11:44 -0300

    Dia 3 Python - Testes

---
+++ Módulo 4/bloco_32/dia_3/codigo.py
@@ -0,0 +1,3 @@
+def is_even(num):
+  "Retorna TRUE se um numero é verdadeiro, senão FALSE"
+  return num % 2 == 0
---
+++ Módulo 4/bloco_32/dia_3/teste_codigo.py
@@ -0,0 +1,6 @@
+from codigo import is_even
+
+def test_is_even_if_number_is_even_return_true():
+  assert is_even(2) is True
+
+  
